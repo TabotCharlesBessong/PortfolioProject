e28b72f28271bc62167af33430bdde9cb5e52af5
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-24 22:07:17 +0100

    pie chart reusable component

---
+++ school_management_system/client_ui/src/component/CustomPieChart.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
+
+const COLORS = ["green", "red", "yellow", "blue", "gray", "black", "orange"];
+
+const RADIAN = Math.PI / 180;
+
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+const CustomPieChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width={730} height={400} >
+      <PieChart>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          outerRadius={80}
+          fill="#8884d8"
+          label={renderCustomizedLabel}
+          width={500}
+          height={350}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomPieChart;
---
+++ school_management_system/client_ui/src/component/index.js
@@ -0,0 +1,2 @@
+export { default as CustomPieChart } from "./CustomPieChart";
+
