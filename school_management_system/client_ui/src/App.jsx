794ec2189c1e29299975d120b423b5fb6024472e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-25 21:47:24 +0100

    implemented the toggle component

--- school_management_system/client_ui/src/App.jsx
+++ school_management_system/client_ui/src/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { CustomBarChart, CustomPieChart } from './component';
+import React, { useState } from 'react';
+import { CustomBarChart, CustomPieChart, Popup } from './component';
 import { barData, generateSubjectData, pieData } from './constant/data/chart';
 
 const App = () => {
   const data = generateSubjectData(8)
   console.log(data)
   console.log(barData)
+  const [showPopup, setShowPopup] = useState(false);
   
   return (
     <div style={{ width: "100%" }}>
       <CustomPieChart data={pieData} />
       <CustomBarChart chartData={data} dataKey="attendancePercentage" />
       <CustomBarChart chartData={barData} dataKey="marksObtained" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <button onClick={() => setShowPopup(!showPopup)}>Toogle</button>
+        <Popup message="Done Successfully!" setShowPopup={setShowPopup} showPopup={showPopup} />
+      </div>
     </div>
   );
 }
--- school_management_system/client_ui/src/component/index.js
+++ school_management_system/client_ui/src/component/index.js
@@ -1,2 +1,3 @@
 export { default as CustomPieChart } from "./CustomPieChart";
 export { default as CustomBarChart } from "./CustomBarChart";
+export {default as Popup} from "./Popup"
