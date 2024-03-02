7d0b2d204fd8269900961da3080a6e8c243937d0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-02 16:45:06 +0100

    signup page

--- blogging/frontend_ui/src/App.jsx
+++ blogging/frontend_ui/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import { Header } from "./components";
+import { SignUp } from "./pages";
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Route path="/" element={"Home page"} />
         <Route path="/about" element={"About page"} />
         <Route path="/signin" element={"Signin page"} />
-        <Route path="/signup" element={"Signup page"} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={"Dashboard"} />
         <Route path="/projects" element={"Projects Page"} />
       </Routes>
