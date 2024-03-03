9fcef6d3efa7860a9601e94abc7f36e0238a43e2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-03 15:24:11 +0100

    signin

--- blogging/api/src/router/user.router.js
+++ blogging/api/src/router/user.router.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
-import { signup } from "../controllers/auth.controller.js"
+import { signin, signup } from "../controllers/auth.controller.js"
 
 const router = Router()
 
 
 router.post("/signup",signup)
+router.post("/signin",signin)
 
 export default router
