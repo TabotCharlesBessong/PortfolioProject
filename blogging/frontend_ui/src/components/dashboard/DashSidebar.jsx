867f534f15f0b79a8360e5e6871777e318007083
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-04 18:43:21 +0100

    image upload

--- blogging/frontend_ui/src/components/dashboard/DashSidebar.jsx
+++ blogging/frontend_ui/src/components/dashboard/DashSidebar.jsx
@@ -21,6 +21,7 @@ const DashSidebar = () => {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
