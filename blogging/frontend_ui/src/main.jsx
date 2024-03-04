6ab5a4eb87e48b7789bebf214b183fb2c0271dac
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-04 10:20:54 +0100

    implemented dark mode

--- blogging/frontend_ui/src/main.jsx
+++ blogging/frontend_ui/src/main.jsx
@@ -5,12 +5,15 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
 import "./index.css";
 import { persistor, store } from "./redux/store.js";
+import { ThemeProvider } from "./components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </Provider>
     </PersistGate>
   </React.StrictMode>
