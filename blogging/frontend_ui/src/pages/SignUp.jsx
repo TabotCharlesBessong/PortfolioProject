723ef48867559cb13ef44142e8c4f7582ec05d63
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-04 09:40:53 +0100

    google sign in with firebase and added user profile picture

--- blogging/frontend_ui/src/pages/SignUp.jsx
+++ blogging/frontend_ui/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {OAuth} from "../components";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
@@ -95,6 +96,7 @@ const SignUp = () => {
                 "Sign Up"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
