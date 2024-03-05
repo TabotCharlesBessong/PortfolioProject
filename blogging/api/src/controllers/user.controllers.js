10a6e81c6cd150e20153a49b67eafee1657a5138
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-05 11:56:02 +0100

    signout api endpoint

--- blogging/api/src/controllers/user.controllers.js
+++ blogging/api/src/controllers/user.controllers.js
@@ -50,12 +50,26 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async (req,res,next) => {
-  if(req.user.id !== req.params.userId) return next(errorHandler(403,"You are not allowed to delete this account"))
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId)
+    return next(
+      errorHandler(403, "You are not allowed to delete this account")
+    );
   try {
-    await User.findByIdAndDelete(req.params.userId)
-    res.status(200).json("User has been deleted")
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const signout = (req, res, next) => {
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User signout successfully");
+  } catch (error) {
+    next(error);
+  }
+};
--- blogging/api/src/router/user.router.js
+++ blogging/api/src/router/user.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { google, signin, signup } from "../controllers/auth.controller.js";
-import { deleteUser, updateUser } from "../controllers/user.controllers.js";
+import { deleteUser, signout, updateUser } from "../controllers/user.controllers.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = Router();
@@ -10,5 +10,6 @@ router.post("/signin", signin);
 router.post("/google", google);
 router.put("/update/:userId",verifyToken,updateUser)
 router.delete("/delete/:userId",verifyToken,deleteUser)
+router.post("/signout",signout)
 
 export default router;
