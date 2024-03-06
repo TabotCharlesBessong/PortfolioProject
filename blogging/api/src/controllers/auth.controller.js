e74ba8c995c4a856ab7a75a3939ffd0009fbadfb
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-06 15:35:06 +0100

    adjusting file location of the signout method

--- blogging/api/src/controllers/auth.controller.js
+++ blogging/api/src/controllers/auth.controller.js
@@ -102,3 +102,14 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signout = (req, res, next) => {
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User signout successfully");
+  } catch (error) {
+    next(error);
+  }
+};
