687ee35653a0a5332edf99678159b4365358c3f2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-06 14:01:08 +0100

    get post api endpoints

--- blogging/api/src/router/post.router.js
+++ blogging/api/src/router/post.router.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { create } from "../controllers/post.controller.js"
+import { create, getPosts } from "../controllers/post.controller.js"
 import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router()
 
 router.post("/create",verifyToken,create)
+router.get("/getposts",getPosts)
 
 export default router
