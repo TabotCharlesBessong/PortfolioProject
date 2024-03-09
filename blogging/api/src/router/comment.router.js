4a9acd0fa6b9177e1552baa93d7a57dfa8ac5501
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-09 16:25:36 +0100

    like a comment api endpoint

--- blogging/api/src/router/comment.router.js
+++ blogging/api/src/router/comment.router.js
@@ -3,11 +3,13 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
   getPostComments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getcomments/:postId", getPostComments);
+router.put("/like/:commentId", verifyToken, likeComment);
 
 export default router;
