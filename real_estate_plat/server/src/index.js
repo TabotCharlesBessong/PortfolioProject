6b7b2a62bafce653d4d6e052548beb20c69b34d3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-03 22:07:56 +0100

    setting up index.js

--- real_estate_plat/server/src/index.js
+++ real_estate_plat/server/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!")
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(express.json())
+app.use(cookieParser)
+
+app.listen(5000,() => {
+  console.log(`Our server is running on port number ${port}...`)
+})
