9d55f26ee287b8a232968a07a52ac4439eb7ee18
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-19 13:13:56 +0100

    completed list page and displayed properties in cards

--- real_estate_plat/estate_ui/src/components/index.js
+++ real_estate_plat/estate_ui/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as SearchBar } from "./searchBar/SearchBar";
+export { default as Card } from "./card/Card";
--- real_estate_plat/estate_ui/src/page/listPage/ListPage.jsx
+++ real_estate_plat/estate_ui/src/page/listPage/ListPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { listData } from '../../constant/data'
+import { Card } from '../../components'
+import "./listPage.scss"
 
 const ListPage = () => {
   return (
-    <div>ListPage</div>
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          {listData.map(item =>  (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+
+      </div>
+    </div>
   )
 }
 
