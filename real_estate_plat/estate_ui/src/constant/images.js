91504f39d59420188a33fce0824abad515544f2e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-19 12:23:09 +0100

    designed the search bar

--- real_estate_plat/estate_ui/src/constant/images.js
+++ real_estate_plat/estate_ui/src/constant/images.js
@@ -1,11 +1,13 @@
 import logo from "../assets/images/logo.png"
 import menu from "../assets/images/menu.png"
 import bg from "../assets/images/bg.png"
+import search from "../assets/images/search.png"
 
 const images = {
   logo,
   menu,
-  bg
+  bg,
+  search
 }
 
 export default images
