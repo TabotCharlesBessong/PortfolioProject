3170c32441f197ed938f2e0c7c4c0c80bef67c93
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-19 13:54:03 +0100

    created the map component

--- real_estate_plat/estate_ui/src/components/index.js
+++ real_estate_plat/estate_ui/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as SearchBar } from "./searchBar/SearchBar";
 export { default as Card } from "./card/Card";
+export { default as Map } from "./map/Map";
