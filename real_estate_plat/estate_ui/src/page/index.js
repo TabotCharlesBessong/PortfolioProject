72a32e1a0b98c4a9e153311cce48470c77a128b1
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-20 09:00:27 +0100

    profile page skeleton

--- real_estate_plat/estate_ui/src/page/index.js
+++ real_estate_plat/estate_ui/src/page/index.js
@@ -1,4 +1,4 @@
-export { default as ProfilePage } from "./ProfilePage";
+export { default as ProfilePage } from "./profilePage/ProfilePage";
 export { default as HomePage } from "./homepage/HomePage";
 export { default as ListPage } from "./listPage/ListPage";
 export { default as DetailPage } from "./detailPage/DetailPage";
