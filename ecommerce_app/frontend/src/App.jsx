383655d5c185383e011b85465d4c3ccf50da709e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-27 05:22:58 +0100

    bootstrapping the project

---
+++ ecommerce_app/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <div className="w-full flex flex-1 items-center justify-center">
+        <h1 className="text-5xl text-center text-orange-700">Hello test</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
