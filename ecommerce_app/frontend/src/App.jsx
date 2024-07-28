d50fa1db7d90fd58574d057a5e6e301c0ae3fa62
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-28 05:07:51 +0100

    created the login page

--- ecommerce_app/frontend/src/App.jsx
+++ ecommerce_app/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import { Login } from "./pages";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element="Hello home" />
-        <Route path="/login" element="Hello Login" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ ecommerce_app/frontend/src/pages/index.js
@@ -0,0 +1,1 @@
+export {default as Login} from "./auth/Login"
