e1caaf1232e56a1a208277e11b01d7e001dea167
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-29 13:25:39 +0100

    best deals

--- ecommerce_app/frontend/src/components/index.js
+++ ecommerce_app/frontend/src/components/index.js
@@ -6,3 +6,5 @@ export { default as DropDown } from "./common/DropDown";
 export { default as Hero } from "./home/Hero";
 export { default as Categories } from "./home/Categories";
 export { default as BestDeals } from "./home/BestDeals";
+export {default as ProductDetailsCard} from "./common/ProductDetailsCard"
+export {default as ProductCard} from "./common/ProductCard"
