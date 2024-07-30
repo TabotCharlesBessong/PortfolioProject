59e3feb3d88e791e05b5a91b3cbf6dcd1f82e868
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-30 10:46:18 +0100

    updating classes

--- school_management_system/school_ui/src/pages/index.js
+++ school_management_system/school_ui/src/pages/index.js
@@ -5,7 +5,7 @@ export { default as Home } from "./common/Home";
 export { default as AdminDashboard } from "./dashboard/admin/AdminDashboard.jsx";
 export { default as Assignments } from "./dashboard/admin/Assignments.jsx";
 export { default as Attendance } from "./dashboard/admin/Attendance.jsx";
-export { default as Classes } from "./dashboard/Classes.jsx";
+export { default as Classes } from "./dashboard/admin/Classes.jsx";
 export { default as StudentDashboard } from "./dashboard/StudentDashboard.jsx";
 export { default as TeacherDashboard } from "./dashboard/TeacherDashboard.jsx";
 
