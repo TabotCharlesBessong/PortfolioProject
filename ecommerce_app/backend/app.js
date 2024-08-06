c63c4924236cfe55228dfcf77afc56ce9600ea12
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-08-06 12:04:39 +0100

    created shop model

--- ecommerce_app/backend/app.js
+++ ecommerce_app/backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const userRouter = require("./router/user.router");
+const shopRouter = require("./router/shop.router");
 
 const app = express();
 dotenv.config();
@@ -18,5 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 // app.use(ErrorHandler)
 
 app.use("/api/user", userRouter);
+app.use("/api/shop", shopRouter);
 
 module.exports = app;
