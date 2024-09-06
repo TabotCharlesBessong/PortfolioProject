03a96a37cfb4801aa583e62810c16dc22f435443
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-09-06 03:56:37 +0100

    middleware function

---
+++ hms_app/app_server/src/middlewares/checkAdmin.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+
+// Extend the Request interface to include the user object
+interface AuthenticatedRequest extends Request {
+  user: {
+    role: string;
+  };
+}
+
+const checkAdmin = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Response | void => {
+  if (req.user.role !== "admin") {
+    return res
+      .status(401)
+      .json({ error: "You Are Not Authorized to perform this Operation!!!" });
+  }
+  next();
+};
+
+export default checkAdmin;
---
+++ hms_app/app_server/src/middlewares/errorHandler.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+const errorHandler = (err:TypeError, req:Request, res:Response, next:NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+};
+
+module.exports = errorHandler;
